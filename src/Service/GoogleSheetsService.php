4e80dac3c727af53c212bfbfb4163ec9903336b7
patriro <olaripatrice@gmail.com>
2019-03-06 22:56:47 +0000

    refacto te functino

--- src/Service/GoogleSheetsService.php
+++ src/Service/GoogleSheetsService.php
@@ -8,28 +8,85 @@ use Google_Service_Sheets;
 
 class GoogleSheetsService
 {
-    private $rootForGoogleCredentials;
-
-    public function __construct($rootForGoogleCredentials)
-    {
-        $this->rootForGoogleCredentials = $rootForGoogleCredentials;
-    }
+    const API_KEY = 'AIzaSyCxajr8iBJZ3iRIiccUOwtiypwdYZnwdUg';
+    const SHEET_ID = '17j2pGpXEtWodVnvxRSiNm-w6YJr_Em8NUl_NL3SuQfw';
 
     private function initalizeGoogleService()
     {
         $client = new Google_Client();
 
-        $client->setAuthConfig($this->rootForGoogleCredentials);
+        $client->setDeveloperKey(self::API_KEY);
+        $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
 
         return new Google_Service_Sheets($client);
     }
 
-    public function getAllGuests()
+    public function saveAllGuestsFromGoogle()
     {
-        $googleSheetsService = $this->initalizeGoogleService();
+        try {
+            $googleSheetsService = $this->initalizeGoogleService();
+        } catch (Exception $e) {
+            throw new Exception("Google Sheet API isn\'t available");
+        }
 
-        dd($googleSheetsService);
+        $datas = $this->getDataFromServer($googleSheetsService);
+        $this->persistInBDD();
+        // 1 Numéro
+        // 2 Nom de Famille
+        // 3 Prénom
+        // 4 Enfant sous 7 ans
+        // 5 Présence
+    }
 
+    private function getDataFromServer()
+    {
+
+        $allHeaders = $this->getHeadersAndSheets();
+        $allDatas = [];
+
+        foreach ($allHeaders as $key => $headers) {
+            $allDatas[$key] = [];
+            foreach ($headers as $sheet) {
+                $datas          = [];
+                $valueRange     = $googleSheetsService->spreadsheets_values->get(self::SHEET_ID, $sheet);
+                $datas          = $this->getDatasWihtoutLinesSpaces($valueRange->getValues());
+                $allDatas[$key] = array_merge($allDatas[$key], $datas);
+            }
+        }
+
+        return $allDatas;
+    }
+
+    private function getHeadersAndSheets()
+    {
+        return [
+            '5headers' => [
+                'burdaSheetsRange'          => 'Familia Burda!A5:E',
+                'OprisiuSheetsRange'        => 'Familia Oprisiu!A5:E',
+                'burdaFriendsSheetsRange'   => 'Prietenii - familistii Burda!A5:E',
+                'olariSheetsRange'          => 'Familia Olari!A5:E',
+                'purtanSheetsRange'         => 'Familia Purtan!A5:E',
+                'closeOlariSheetsRange'     => 'Familia apropiata Olari!A5:E',
+                'olariFriendsSheetsRange'   => 'Prietenii - familistii Olari!A5:E',
+                'ComitetSheetsRange'        => 'Comitet!A5:E',
+                'FrenchSheetsRange'         => 'Francezi!A5:E',
+            ],
+            '4headers' => [
+                'teensSheetsRange'          => 'Tinerii!A5:D',
+            ],
+        ];
+    }
+
+    private function getDatasWihtoutLinesSpaces($datas)
+    {
+        foreach ($datas as $key => $value) {
+            if (empty($value)) {
+                array_splice($datas, $key);
+                continue;
+            }
+        }
+
+        return $datas;
     }
 
 }
