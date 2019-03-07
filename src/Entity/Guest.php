3a372388eb063a7c2c5a71284f20d59230e74b4b
patriro <olaripatrice@gmail.com>
2019-03-07 19:32:43 +0000

    adding Guest ENtity

---
+++ src/Entity/Guest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GuestRepository")
+ */
+class Guest
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $childUnder7;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $presence;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getChildUnder7(): ?bool
+    {
+        return $this->childUnder7;
+    }
+
+    public function setChildUnder7(?bool $childUnder7): self
+    {
+        $this->childUnder7 = $childUnder7;
+
+        return $this;
+    }
+
+    public function getPresence(): ?bool
+    {
+        return $this->presence;
+    }
+
+    public function setPresence(bool $presence): self
+    {
+        $this->presence = $presence;
+
+        return $this;
+    }
+}
